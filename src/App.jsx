882071362fe8b213c729b028dfb624c8f55223f2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-17 23:15:07 -0500

    Render Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import Title from "./Title";
+import { useState } from 'react';
+
+import menu from './data';
+
+import Title from './Title';
+import Menu from './Menu';
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+
   return (
     <section className="menu">
       <Title text="our menu" />
+      <Menu items={menuItems} />
     </section>
   );
 };
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,7 @@
 const Title = ({ text }) => {
   return (
     <div className="title">
-      <h2>{text || "Default Title"}</h2>
+      <h2>{text || 'Default Title'}</h2>
       <div className="title-underline"></div>
     </div>
   );
