8c7bae722c8fa93707e9bb683a44d161f2bdbec4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-18 00:50:45 -0500

    Filter Items

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import menu from './data';
 
 import Title from './Title';
+import Categories from './Categories';
 import Menu from './Menu';
 
 const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
@@ -11,11 +12,19 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
 
-  console.log(categories);
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <section className="menu">
       <Title text="our menu" />
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems} />
     </section>
   );
