92dab36b4eb97529f703846ce1ddb44833b06a0e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-17 23:55:26 -0500

    Unique Categories

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import menu from './data';
 import Title from './Title';
 import Menu from './Menu';
 
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  console.log(categories);
 
   return (
     <section className="menu">
